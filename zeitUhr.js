68655d3b60ba69b4905cc9c3706f579dcbec7f33
BubJla <123741924+BubJla@users.noreply.github.com>
2023-02-07 20:52:56 +0100

    millisecond hand

--- zeitUhr.js
+++ zeitUhr.js
@@ -23,11 +23,12 @@ function zeit(){
 
 
 
-setInterval(setClock, 10)
+setInterval(setClock, 1)
 
 const hourHand = document.querySelector('[data-hour-hand]')
 const minuteHand = document.querySelector('[data-minute-hand]')
 const secondHand = document.querySelector('[data-second-hand]')
+const millisecondHand = document.querySelector('[data-millisecond-hand]')
 
 function setClock() {
   const currentDate = new Date()
@@ -35,6 +36,7 @@ function setClock() {
   const secondsRatio = (millisecondsRatio + currentDate.getSeconds()) / 60
   const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
   const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+  setRotation(millisecondHand, millisecondsRatio)
   setRotation(secondHand, secondsRatio)
   setRotation(minuteHand, minutesRatio)
   setRotation(hourHand, hoursRatio)
