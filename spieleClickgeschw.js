af09b2ffcb7a1fe07c6058aa49b166d3017f0fcd
BubJla <123741924+BubJla@users.noreply.github.com>
2023-02-26 17:43:03 +0100

    Clickgeschwindigkeit

---
+++ spieleClickgeschw.js
@@ -0,0 +1,29 @@
+var endzeit = 0;
+var startet = 0;
+var interval
+var clicks;
+var zeitS = 10;
+var anfangszeit;
+
+function starting(){
+    endzeit = new Date().getTime() + zeitS*1000;
+    anfangszeit = new Date().getTime();
+    startet = 1;
+    clicks = 0;
+    interval = setInterval(function() {ueberpruefen();}, 1);
+    document.getElementById("startbutton").setAttribute("disabled", "");
+}
+
+function ueberpruefen() {
+    var jetzt = new Date().getTime();
+    if(jetzt > endzeit && endzeit != 0) {
+        ausgabe();
+        clearInterval(interval);
+    }
+}
+
+function ausgabe() {
+    var zeit = (new Date().getTime() - anfangszeit)/1000;
+    document.getElementById("clickfeld").setAttribute("class", "white");
+    document.getElementById("werte").innerHTML = "Zeit: " + Math.round(zeit) +"s<br>" + "Clicks: "+clicks + " <br>Clicks/sec: "+Math.round(clicks/zeit*19)/10;
+}
