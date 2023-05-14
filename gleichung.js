c02045dd66d2559d530b01a3b8c17debe877d12b
BubJla <123741924+BubJla@users.noreply.github.com>
2023-05-14 11:25:17 +0200

    gleichungslöser verbessert(fehler - -Vorzeichen)

--- gleichung.js
+++ gleichung.js
@@ -1,11 +1,83 @@
+function ersetzen(text, zuErsetzen, ersetzwert) {
+    var ergebnis = text;
+    for(let n = 0; n < text.length; n++){
+        ergebnis = ergebnis.replace(zuErsetzen, ersetzwert);
+    }
+    return ergebnis;
+}
+
+
+function gleichungGenau(term, grenzwert1, grenzwert2) {
+    var ergebnis;
+    var i = 0;
+    while(1) {
+        ergebnis = (grenzwert1+grenzwert2)/2;
+        i++;
+        if(Math.abs(eval(ersetzen(term, "x", ergebnis))) < 1/10000000000 || i > 100) return ergebnis.toFixed(9);
+        if(eval(ersetzen(term, "x", (grenzwert1+grenzwert2)/2)) == 0) return ((grenzwert1+grenzwert2)/2).toFixed(6);
+        else if(eval(ersetzen(term, "x", (grenzwert1+grenzwert2)/2))*eval(ersetzen(term, "x", grenzwert1)) < 0) {
+            grenzwert2 = (grenzwert1+grenzwert2)/2;
+        }
+        else if(eval(ersetzen(term, "x", (grenzwert1+grenzwert2)/2))*eval(ersetzen(term, "x", grenzwert2)) < 0) {
+            grenzwert1 = (grenzwert1+grenzwert2)/2;
+        }
+        else return "null";
+    }
+}
+
+
 function gleichung() {
     var links = document.getElementById("seiteL").value;
     var rechts = document.getElementById("seiteR").value;
     var ergebnisse = [];
-    var termL = links.replace("X", "x").replace("0", 0).replace("1", 1).replace("2", 2).replace("3", 3).replace("4", 4).replace("5", 5).replace("6", 6).replace("7", 7).replace("8", 8).replace("9", 9).replace("π", "Math.PI").replace("e", "Math.E").replace("÷", "/").replace("×", "*").replace("²", "**2").replace("³", "**3").replace("^", "**");
-    for(let i = 0; i < termL.length; i++) termL = termL.replace("X", "x").replace("0", 0).replace("1", 1).replace("2", 2).replace("3", 3).replace("4", 4).replace("5", 5).replace("6", 6).replace("7", 7).replace("8", 8).replace("9", 9).replace("π", "Math.PI").replace("e", "Math.E").replace("÷", "/").replace("×", "*").replace("²", "**2").replace("³", "**3").replace("^", "**");
-    var termR = rechts.replace("X", "x").replace("0", 0).replace("1", 1).replace("2", 2).replace("3", 3).replace("4", 4).replace("5", 5).replace("6", 6).replace("7", 7).replace("8", 8).replace("9", 9).replace("π", "Math.PI").replace("e", "Math.E").replace("÷", "/").replace("×", "*").replace("²", "**2").replace("³", "**3").replace("^", "**");
-    for(let i = 0; i < termR.length; i++) termR = termR.replace("X", "x").replace("0", 0).replace("1", 1).replace("2", 2).replace("3", 3).replace("4", 4).replace("5", 5).replace("6", 6).replace("7", 7).replace("8", 8).replace("9", 9).replace("π", "Math.PI").replace("e", "Math.E").replace("÷", "/").replace("×", "*").replace("²", "**2").replace("³", "**3").replace("^", "**");
+    var termL = links;
+    for(let i = 0; i < termL.length; i++) termL = termL.replace("X", "x").replace("-", "(-1)*").replace("x²", "x*x").replace("x^2", "x*x").replace("x³", "x*x*x").replace("x^3", "x*x*x").replace("0", 0).replace("1", 1).replace("2", 2).replace("3", 3).replace("4", 4).replace("5", 5).replace("6", 6).replace("7", 7).replace("8", 8).replace("9", 9).replace("π", "Math.PI").replace("e", "Math.E").replace("÷", "/").replace("×", "*").replace("²", "**2").replace("³", "**3").replace("^", "**");
+    var termR = rechts;
+    for(let i = 0; i < termR.length; i++) termR = termR.replace("X", "x").replace("-", "-").replace("x²", "x*x").replace("x^2", "x*x").replace("x³", "x*x*x").replace("x^3", "x*x*x").replace("0", 0).replace("1", 1).replace("2", 2).replace("3", 3).replace("4", 4).replace("5", 5).replace("6", 6).replace("7", 7).replace("8", 8).replace("9", 9).replace("π", "Math.PI").replace("e", "Math.E").replace("÷", "/").replace("×", "*").replace("²", "**2").replace("³", "**3").replace("^", "**");
+    var x;
+    var ergebnisse = [];
+    var unendlich = 0;
+    var grenzeP = 0;
+    var grenzeN = 0;
+
+    for(x = 0; x < 100; x++) {
+        unendlich += eval(termL) == eval(termR);
+    }
+    if(unendlich == 100) {
+        document.getElementById("ergebnisGleichung").value = "unendlich viele Lösungen";
+        return;
+    }
+    if(eval(ersetzen(termL, "x", 0)) == eval(ersetzen(termR, "x", 0))) ergebnisse[0] = 0;
+    for(var i = 0.1; i < 999999999999999; i*=1.5) {
+        console.log(i);
+        if(((eval(ersetzen(termL, "x", "i"))-eval(ersetzen(termR, "x", "i")))*(eval(ersetzen(termL, "x", "grenzeP"))-eval(ersetzen(termR, "x", "grenzeP")) )) < 0) {
+            let term = termL+"-"+termR;
+            let ergebnis = gleichungGenau(term, grenzeP, i);
+            if(ergebnis != "null") ergebnisse[ergebnisse.length] = ergebnis;
+        }
+        if(((eval(ersetzen(termL, "x", "-1*i"))-eval(ersetzen(termR, "x", "-1*i")))*(eval(ersetzen(termL, "x", "grenzeN"))-eval(ersetzen(termR, "x", "grenzeN")) )) < 0) {
+            let term = termL+"-"+termR;
+            let ergebnis = gleichungGenau(term, grenzeN, -i);
+            if(ergebnis != "null") ergebnisse[ergebnisse.length] = ergebnis;
+        }
+        grenzeP = i;
+        grenzeN = -i;
+    }
+    if(ergebnisse=="") ergebnisse = "keine oder zu große Lösung";
+    document.getElementById("ergebnisGleichung").value = ergebnisse;
+    document.getElementById("ergebnisGleichung").value = document.getElementById("ergebnisGleichung").value.replace(",", " / ");
+}
+
+
+
+function gleichung_alt() {
+    var links = document.getElementById("seiteL").value;
+    var rechts = document.getElementById("seiteR").value;
+    var ergebnisse = [];
+    var termL = links.replace("X", "x").replace("-", "-1*").replace("0", 0).replace("1", 1).replace("2", 2).replace("3", 3).replace("4", 4).replace("5", 5).replace("6", 6).replace("7", 7).replace("8", 8).replace("9", 9).replace("Ï€", "Math.PI").replace("e", "Math.E").replace("Ã·", "/").replace("Ã—", "*").replace("Â²", "**2").replace("Â³", "**3").replace("^", "**");
+    for(let i = 0; i < termL.length; i++) termL = termL.replace("X", "x").replace("0", 0).replace("1", 1).replace("2", 2).replace("3", 3).replace("4", 4).replace("5", 5).replace("6", 6).replace("7", 7).replace("8", 8).replace("9", 9).replace("Ï€", "Math.PI").replace("e", "Math.E").replace("Ã·", "/").replace("Ã—", "*").replace("Â²", "**2").replace("Â³", "**3").replace("^", "**");
+    var termR = rechts.replace("X", "x").replace("0", 0).replace("1", 1).replace("2", 2).replace("3", 3).replace("4", 4).replace("5", 5).replace("6", 6).replace("7", 7).replace("8", 8).replace("9", 9).replace("Ï€", "Math.PI").replace("e", "Math.E").replace("Ã·", "/").replace("Ã—", "*").replace("Â²", "**2").replace("Â³", "**3").replace("^", "**");
+    for(let i = 0; i < termR.length; i++) termR = termR.replace("X", "x").replace("0", 0).replace("1", 1).replace("2", 2).replace("3", 3).replace("4", 4).replace("5", 5).replace("6", 6).replace("7", 7).replace("8", 8).replace("9", 9).replace("Ï€", "Math.PI").replace("e", "Math.E").replace("Ã·", "/").replace("Ã—", "*").replace("Â²", "**2").replace("Â³", "**3").replace("^", "**");
     var unterschied;
     var unterschiedLetzt = 999999999999;
     var werte = [];
@@ -17,7 +89,7 @@ function gleichung() {
         unendlich += eval(termL) == eval(termR);
     }
     if(unendlich == 100) {
-        document.getElementById("ergebnisGleichung").value = "unendlich viele Lösungen";
+        document.getElementById("ergebnisGleichung").value = "unendlich viele LÃ¶sungen";
         return;
     }
 
@@ -54,10 +126,3 @@ function gleichung() {
     document.getElementById("ergebnisGleichung").value = ergebnisse;
     document.getElementById("ergebnisGleichung").value = document.getElementById("ergebnisGleichung").value.replace(",", " / ");
 }
-
-//alert(Math.abs(eval(1/0)-eval(1/0)));
-//alert(1/0==1/0);
-
-//alert(ergebnisse.length);
-
-//gleichung();
