555e25e2078fc412f2796c48faba49ac2a5bd27e
BubJla <julian@brumbi.de>
2023-10-21 10:49:26 +0200

    matomo anstatt google analytics

--- start.js
+++ start.js
@@ -7,13 +7,25 @@ document.head.innerHTML += `
 <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
 <link rel="manifest" href="/site.webmanifest">
 
-<script src="https://www.googletagmanager.com/gtag/js?id=G-QPH30E9YBE"></script>
+
+<!-- Matomo -->
 <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-QPH30E9YBE');
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://brumbi0.matomo.cloud/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src='//cdn.matomo.cloud/brumbi0.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
 </script>
+<!-- End Matomo Code -->
+
+
+
 
 <link rel="alternate" href="de.html" hreflang="de">
   <link rel="alternate" href="en.html" hreflang="en">
